f47bcd53ca68cc906d105f6a7aff12b789fb0ff2
Yun Jang <jang.yunj7@gmail.com>
2023-04-15 21:46:32 -0400

    second commit

--- main.cpp
+++ main.cpp
@@ -19,19 +19,19 @@ int main()
 
 void drawBoard(char *spaces)
 {
-    std::cout << "     |     |     |" << '\n';
-    std::cout << "   " << spaces[0] << " | " << spaces[1] << " | " << spaces[2] << " | " << '\n';
-    std::cout << "_____|_____|_____|" << '\n';
+    std::cout << '\n';
+    std::cout << "     |     |     " << '\n';
+    std::cout << "  " << spaces[0] << "  |  " << spaces[1] << "  |  " << spaces[2] << "   " << '\n';
+    std::cout << "_____|_____|_____" << '\n';
+    std::cout << "     |     |     " << '\n';
+    std::cout << "  " << spaces[3] << "  |  " << spaces[4] << "  |  " << spaces[5] << "   " << '\n';
+    std::cout << "_____|_____|_____" << '\n';
+    std::cout << "     |     |     " << '\n';
+    std::cout << "  " << spaces[6] << "  |  " << spaces[7] << "  |  " << spaces[8] << "   " << '\n';
+    std::cout << "     |     |     " << '\n';
+    std::cout << '\n';
 }
-void playerMove(char *spaces, char player)
-{
-}
-void computerMove(char *spaces, char computer)
-{
-}
-bool checkWinner(char *spaces, char player, char computer)
-{
-}
-bool checkTie(char *spaces)
-{
-}
+void playerMove(char *spaces, char player);
+void computerMove(char *spaces, char computer);
+bool checkWinner(char *spaces, char player, char computer);
+bool checkTie(char *spaces);
